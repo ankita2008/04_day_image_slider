648e108da8d96561e11c56b91e92c56b412587f5
ankita2008 <satasiyaankita33@gmail.com>
2026-05-04 14:33:20 +0530

    image slider

---
+++ script.js
@@ -0,0 +1,57 @@
+const galleryContainer = document.querySelector('.gallery-container');
+const galleryControlsContainer = document.querySelector('.gallery-controls');
+const galleryControls = ['previous', 'next'];
+const galleryItems = document.querySelectorAll('.gallery-item');
+
+class Carousel{
+    constructor(container, items, controls){
+        this.CarouselContainer = container;
+        this.CarouselControls = controls;
+        this.CarouselArray = [...items];
+    }
+
+    updateGallery(){
+        this.CarouselArray.forEach(el =>{
+            el.classList.remove('gallery-item-1');
+            el.classList.remove('gallery-item-2');
+            el.classList.remove('gallery-item-3');
+            el.classList.remove('gallery-item-4');
+            el.classList.remove('gallery-item-5');
+        });
+
+        this.CarouselArray.slice(0,5).forEach((el , i) => {
+            el.classList.add(`gallery-item-${i+1}`);
+        });
+    }
+
+    setCurrentState(direction){
+        if(direction.className == 'gallery-controls-previous'){
+            this.CarouselArray.unshift(this.CarouselArray.pop());
+        }else{
+            this.CarouselArray.push(this.CarouselArray.shift());
+        }
+        this.updateGallery();
+    }
+
+    setControls(){
+        this.CarouselControls.forEach(control => {
+            galleryControlsContainer.appendChild(document.createElement('button')).className = `gallery-controls-${control}`;
+            document.querySelector(`.gallery-controls-${control}`).innerText = control;
+        });
+    }
+
+    useControls(){
+        const triggers = [...galleryControlsContainer.children];
+        triggers.forEach(control => {
+            control.addEventListener('click', e => {
+                e.preventDefault();
+                this.setCurrentState(control);
+            });
+        });
+    }
+}
+
+const exampleCarousel = new Carousel(galleryContainer, galleryItems, galleryControls);
+
+exampleCarousel.setControls();
+exampleCarousel.useControls();
